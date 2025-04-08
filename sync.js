e7e7f9fe0b53a61d8f5fa760490d5592ebb79807
mrkrisworld <louis6198@yahoo.com>
2025-04-08 18:28:27 +0900

    Add support for attachments forwarding. Use Discord nickname instead of username.

--- sync.js
+++ sync.js
@@ -28,18 +28,25 @@ discordClient.login(process.env.DISCORD_BOT_TOKEN);
 // Slack Message Event Listener
 slackApp.event("message", async ({ event, client }) => {
     try {
-        if (!event.text
+        if ((!event.text && !event.files)
             || event.subtype === "bot_message"
             || event.bot_id
             || event.channel !== process.env.SLACK_CHANNEL_ID) return;
 
         const userInfo = await client.users.info({ user: event.user });
         const username = userInfo.user.real_name || userInfo.user.name;
-        const message = event.text;
+        const message = event.text || "";
+
+        let fileLinks = "";
+        if (event.files && event.files.length > 0) {
+            console.log("start link compile");
+            fileLinks = event.files.map((file) => file.url_private).join("\n");
+            console.log("file link compiled");
+        }
 
         const discordChannel = await discordClient.channels.fetch(process.env.DISCORD_CHANNEL_ID);
         if (discordChannel.isTextBased()) {
-            await discordChannel.send(`**${username}**: ${message}`);
+            await discordChannel.send(`**${username}**: ${message}\n${fileLinks}`);
         }
     } catch (error) {
         console.error("Error handling Slack event:", error);
@@ -53,9 +60,16 @@ discordClient.on("messageCreate", async (message) => {
         if (message.author.id === discordClient.user.id
         || message.channel.id !== process.env.DISCORD_CHANNEL_ID) return; // Ignore only this bot's messages
 
+        const username = message.member?.displayName || message.author.username;
+        let text = `*${username}*: ${message.content}`;
+        if (message.attachments.size > 0) {
+            const attachmentLinks = [...message.attachments.values()].map(att => att.url).join("\n");
+            text += `\n${attachmentLinks}`;
+        }
+
         await slackApp.client.chat.postMessage({
             channel: process.env.SLACK_CHANNEL_ID,
-            text: `*${message.author.username}*: ${message.content}`,
+            text,
         });
     } catch (error) {
         console.error("Error forwarding Discord message to Slack:", error);
